6a0d8899ed5dee2602141997f1aa83274af964b6
Dushyant Patel <dushyantpatel_r@outlook.com>
2018-05-21 11:10:37 -0700

    fixes code redundancy in test_api_resources

--- tests/test_api_resources.py
+++ tests/test_api_resources.py
@@ -16,9 +16,9 @@ num_valid_methods = [1, 4, 4, 4, 3, 0]
 
 class TestAPIResources(unittest.TestCase):
 
-    def test_mai(self):
-        resource = resources[0]
-        num_methods = num_valid_methods[0]
+    def run_tests(self, id):
+        resource = resources[id]
+        num_methods = num_valid_methods[id]
         event = Event()
         event.setPath(resource)
 
@@ -37,94 +37,21 @@ class TestAPIResources(unittest.TestCase):
             res = main_handler(event.getEvent(), None)
             self.assertEqual(res['statusCode'], 501)
             self.assertEqual(res['body'], str(None))
+
+    def test_mai(self):
+        self.run_tests(0)
 
     def test_events(self):
-        resource = resources[1]
-        num_methods = num_valid_methods[1]
-        event = Event()
-        event.setPath(resource)
-
-        # test all valid methods
-        for method in methods[:num_methods]:
-            event.setHttpMethod(method)
-            msg = message_template.format(resource, method.lower())
-            res = main_handler(event.getEvent(), None)
-            self.assertEqual(res['statusCode'], 200)
-            body = ast.literal_eval(res['body'])
-            self.assertEqual(body['message'], msg)
-
-        # test all invalid methods
-        for method in methods[num_methods:]:
-            event.setHttpMethod(method)
-            res = main_handler(event.getEvent(), None)
-            self.assertEqual(res['statusCode'], 501)
-            self.assertEqual(res['body'], str(None))
+        self.run_tests(1)
 
     def test_missions(self):
-        resource = resources[2]
-        num_methods = num_valid_methods[2]
-        event = Event()
-        event.setPath(resource)
-
-        # test all valid methods
-        for method in methods[:num_methods]:
-            event.setHttpMethod(method)
-            msg = message_template.format(resource, method.lower())
-            res = main_handler(event.getEvent(), None)
-            self.assertEqual(res['statusCode'], 200)
-            body = ast.literal_eval(res['body'])
-            self.assertEqual(body['message'], msg)
-
-        # test all invalid methods
-        for method in methods[num_methods:]:
-            event.setHttpMethod(method)
-            res = main_handler(event.getEvent(), None)
-            self.assertEqual(res['statusCode'], 501)
-            self.assertEqual(res['body'], str(None))
+        self.run_tests(2)
 
     def test_plan(self):
-        resource = resources[3]
-        num_methods = num_valid_methods[3]
-        event = Event()
-        event.setPath(resource)
-
-        # test all valid methods
-        for method in methods[:num_methods]:
-            event.setHttpMethod(method)
-            msg = message_template.format(resource, method.lower())
-            res = main_handler(event.getEvent(), None)
-            self.assertEqual(res['statusCode'], 200)
-            body = ast.literal_eval(res['body'])
-            self.assertEqual(body['message'], msg)
-
-        # test all invalid methods
-        for method in methods[num_methods:]:
-            event.setHttpMethod(method)
-            res = main_handler(event.getEvent(), None)
-            self.assertEqual(res['statusCode'], 501)
-            self.assertEqual(res['body'], str(None))
+        self.run_tests(3)
 
     def test_users(self):
-        resource = resources[4]
-        num_methods = num_valid_methods[4]
-        event = Event()
-        event.setPath(resource)
-
-        # test all valid methods
-        for method in methods[:num_methods]:
-            event.setHttpMethod(method)
-            msg = message_template.format(resource, method.lower())
-            res = main_handler(event.getEvent(), None)
-            self.assertEqual(res['statusCode'], 200)
-            body = ast.literal_eval(res['body'])
-            self.assertEqual(body['message'], msg)
-
-        # test all invalid methods
-        for method in methods[num_methods:]:
-            event.setHttpMethod(method)
-            res = main_handler(event.getEvent(), None)
-            self.assertEqual(res['statusCode'], 501)
-            self.assertEqual(res['body'], str(None))
+        self.run_tests(4)
 
 
 if __name__ == '__main__':
