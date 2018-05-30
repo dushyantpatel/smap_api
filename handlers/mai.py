dd02de27e625ddf3f9a18e3f1f320da76c5b9c02
Dushyant Patel <dushyantpatel_r@outlook.com>
2018-05-30 00:47:05 -0700

    Adds capabilities for resource handlers to handle exceptions and return corresponding HTTP response codes

--- handlers/mai.py
+++ handlers/mai.py
@@ -1,5 +1,6 @@
 from response_objects import *
 from handlers.mai_method import *
+from exceptions import *
 import ast
 
 
@@ -9,19 +10,38 @@ methods = {'GET': get}
 
 # handler for mai
 def handler(event, connection):
-    """This function will handle all method requests for mai"""
+    """
+    This function will handle all method requests for mai
+
+    :param event: the api_call event dict object
+    :param connection: an open connection to the database
+    :return: the HTTP response object
+    """
+
     method = event['httpMethod']
     status_code = 200
+    details = 'Everything seems to be OK'
     header = Header()
-    body = Body()
+    res_body = Body()
 
     try:
-        body = methods[method](ast.literal_eval(event['body']), connection)
+        req_body = "{}" if event['body'] is None else event['body']
+        res_body = methods[method](ast.literal_eval(req_body), connection)
     except KeyError:
         status_code = 501
+    except SyntaxError:
+        status_code = 400
+        details = 'Incorrect body format'
+    except HTTP_204_Exception as ex:
+        status_code = 204
+        details = str(ex)
+    except HTTP_400_Exception as ex:
+        status_code = 400
+        details = str(ex)
 
     header.addParameter('status', status_code)
     header.addParameter('message', responseCodeDescription(status_code))
-    res = Response(status_code, header.getHeader(), body.getBody())
+    header.addParameter('details', details)
+    res = Response(status_code, header.getHeader(), res_body.getBody())
 
     return res.getResponse()
