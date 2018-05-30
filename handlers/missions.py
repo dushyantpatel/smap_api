1e721f9e2670d7788594820c6fc86007389d3001
Dushyant Patel <dushyantpatel_r@outlook.com>
2018-05-30 00:47:05 -0700

    Adds capabilities for method handlers to handle exceptions and return corresponding HTTP response codes

--- handlers/missions.py
+++ handlers/missions.py
@@ -1,9 +1,10 @@
 from response_objects import *
 from handlers.missions_method import *
+from exceptions import *
 import ast
 
 
-# list of valid methods for missions
+# dictionary of valid methods for missions
 methods = {'GET': get,
            'POST': post,
            'PUT': put,
@@ -12,19 +13,40 @@ methods = {'GET': get,
 
 # handler for missions
 def handler(event, connection):
-    """This function will handle all method requests for missions"""
+    """
+    This function will handle all method requests for missions
+
+    :param event: the api_call event dict object
+    :param connection: an open connection to the database
+    :return: the HTTP response object
+    """
+
     method = event['httpMethod']
-    status_code = 200
+    status_code = 201
+    details = 'Everything seems to be OK'
     header = Header()
-    body = Body()
+    res_body = Body()
 
     try:
-        body = methods[method](ast.literal_eval(event['body']), connection)
+        req_body = "{}" if event['body'] is None else event['body']
+        res_body = methods[method](ast.literal_eval(req_body), connection)
+        if method == 'GET':
+            status_code = 200
     except KeyError:
         status_code = 501
+    except SyntaxError:
+        status_code = 400
+        details = 'Incorrect body format'
+    except HTTP_204_Exception as ex:
+        status_code = 204
+        details = str(ex)
+    except HTTP_400_Exception as ex:
+        status_code = 400
+        details = str(ex)
 
     header.addParameter('status', status_code)
     header.addParameter('message', responseCodeDescription(status_code))
-    res = Response(status_code, header.getHeader(), body.getBody())
+    header.addParameter('details', details)
+    res = Response(status_code, header.getHeader(), res_body.getBody())
 
     return res.getResponse()
