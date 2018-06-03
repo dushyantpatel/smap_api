dd0d2fa2bc8615895289ce32dc654040bafe4008
christopherthomas226 <cat044@ucsd.edu>
2018-06-03 13:06:59 -0700

    Gett all events implemented/phase1

--- handlers/events_method/get.py
+++ handlers/events_method/get.py
@@ -1,9 +1,15 @@
 # TODO - implementation
 from response_objects.response_body import Body
+from database_queries import *
+from exceptions import *
 
 
 # NOTE: this function must return a dictionary type
 def get(request, connection):
+    #Get all events from the data_base
+    with connection.cursor() as cur:
+        cur.execute(query_strings.search_all_events)
+        __events = cur.fetchall()
     body = Body()
-    body.addParameter('message', 'events.get has been called')
+    body.addParameter('events', __events)
     return body
