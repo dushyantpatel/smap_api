03f7017508812f622ac7367c6bb897028cb6c964
Sevag <skeossei@ucsd.edu>
2018-06-03 14:31:50 -0700

    changes to friends post and get

--- handlers/friends_method/post.py
+++ handlers/friends_method/post.py
@@ -1,4 +1,3 @@
-# TODO - implementation
 from response_objects.response_body import Body
 from database_queries import *
 from exceptions import *
@@ -8,18 +7,17 @@ import pymysql
 # NOTE: this function must return a dictionary type
 def post(request, connection):
     # connection = pymysql.connect() # DELETE this after done with coding
+
     try:
-        first_user = request['user_1']
-        second_user = request['user_2']
+        __first_user = request['user_1']
+        __second_user = request['user_2']
     except KeyError:
         raise HTTP_400_Exception('Missing required field(s)')
-    try:
-        link = query_strings.link_friend.format[second_user, first_user, 'NULL', first_user, second_user, 'NULL']
-    except:
-        raise HTTP_400_Exception('Invalid user(s)')
+
+    __link = query_strings.link_friend.format[__second_user, __first_user, 'NULL', __first_user, __second_user, 'NULL']
 
     with connection.cursor() as cur:
-        cur.execute(link)
+        cur.execute(__link)
         connection.commit()
 
     body = Body()
