8b3fbfee3f91deb928fd22c1fbd0e38ade029f6c
Dushyant Patel <dushyantpatel_r@outlook.com>
2018-06-03 23:52:34 -0700

    Fixes events/post - unit test works for required fields

--- handlers/events_method/__init__.py
+++ handlers/events_method/__init__.py
@@ -1,5 +1,4 @@
 from handlers.events_method.get import get
-# from handlers.events_method.post import post
-from handlers.events_method.post_temp import post
+from handlers.events_method.post import post
 from handlers.events_method.put import put
 from handlers.events_method.delete import delete
