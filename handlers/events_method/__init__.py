7d6404085f3a12800162a27b609da8a499fb4f2b
Dushyant Patel <dushyantpatel_r@outlook.com>
2018-06-03 22:15:52 -0700

    modifies events_method imports for post_temp.py

--- handlers/events_method/__init__.py
+++ handlers/events_method/__init__.py
@@ -1,4 +1,5 @@
 from handlers.events_method.get import get
-from handlers.events_method.post import post
+# from handlers.events_method.post import post
+from handlers.events_method.post_temp import post
 from handlers.events_method.put import put
 from handlers.events_method.delete import delete
